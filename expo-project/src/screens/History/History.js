c8d63f758f89006893e7b9893d239c1f64d9a336
vilmacio22 <vilmacio22@users.noreply.github.com>
2020-05-04 22:49:43 -0300

    new format

--- expo-project/src/screens/History/History.js
+++ expo-project/src/screens/History/History.js
@@ -1,15 +1,21 @@
-import React, {useState} from 'react'
-import {StyleSheet, View, Text} from 'react-native'
+import React, { useState } from 'react'
+import { StyleSheet, View, FlatList, SafeAreaView, Text, TouchableOpacity } from 'react-native'
 import Header from '../components/Header'
 import AntDesign from 'react-native-vector-icons/AntDesign';
 import Swipeable from "react-native-gesture-handler/Swipeable";
 
 
-export default function History({ navigation }){
-    
+export default function History({ navigation }) {
 
-    return(
-        <View>
+    _onSwipeFromRight = () => {
+        alert('from right');
+    };
+    _onSwipeFromLeft = () => {
+        alert('from Left');
+    };
+
+    return (
+        <SafeAreaView>
             <Header
                 placement="left"
                 backgroundColor="#550bb0"
@@ -23,6 +29,20 @@ export default function History({ navigation }){
                 centerComponent={{ text: 'History', style: { color: '#fff', fontSize: 16 } }}
                 rightComponent={{}}
             />
-        </View>
+            <FlatList
+                data={[1, 2, 3, 4]}
+                style={styles.historyList}
+                keyExtractor={incident => String(incident)}
+                renderItem={() => (<Text style={{color:'#000'}}>EAE FDP</Text>)}
+            />
+
+        </SafeAreaView>
     )
 }
+
+const styles = StyleSheet.create({
+    historyList:{
+        
+    }
+
+})
